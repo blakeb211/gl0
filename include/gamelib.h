986da5076caf6fefe7bd7e7e1a886be34d177de2
Blake Ba <blake.elink@gmail.com>
2021-04-06 00:45:53 -0400

    cleaned up input code

--- include/gamelib.h
+++ include/gamelib.h
@@ -256,7 +256,7 @@ enum Camera_Movement {
 // Default camera values used internally by Camera class
 const float YAW = -90.0f;
 const float PITCH = 0.0f;
-const float SPEED = 2.5f;
+const float SPEED = 0.5f;
 const float SENSITIVITY = 0.1f;
 const float ZOOM = 25.0f;
 
@@ -313,7 +313,7 @@ public:
     // processes input received from any keyboard-like input system. Accepts input parameter in the form of camera defined ENUM (to abstract it from windowing systems)
     void ProcessKeyboard(Camera_Movement direction, float deltaTime)
     {
-        float velocity = MovementSpeed * deltaTime;
+        float velocity = MovementSpeed * deltaTime * SENSITIVITY;
         if (direction == FORWARD)
             Position += Front * velocity;
         if (direction == BACKWARD)
--- src/main.cpp
+++ src/main.cpp
@@ -8,41 +8,46 @@
 #include "headers.h"
 
 void framebuf_size_callback(GLFWwindow* window, int width, int height);
-void processInput(GLFWwindow* window, gxb::Camera& cam);
+void processInput(GLFWwindow* window, gxb::Camera& cam, float deltaTime);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 void init_textures();
 unsigned int init_vertices();
 void logOpenGLInfo();
-GLFWwindow* initGLFW(unsigned int w, unsigned int h, const char* title, GLFWframebuffersizefun);
+GLFWwindow* initGLFW(unsigned int w,
+		     unsigned int h,
+		     const char* title,
+		     GLFWframebuffersizefun);
 
-gxb::Camera camera {};
+// GLOBALS
+// -------------------------------------------
+gxb::Camera camera{};
 float lastX = gxb::SCR_WIDTH / 2, lastY = gxb::SCR_HEIGHT / 2;
 bool firstMouse = true;
 
-int main()
-{
-    gxb::initTypeToStrMap(); // creates str_to_type 
+int main() {
+    gxb::initTypeToStrMap();  // creates str_to_type
 
     FrameRater fr{};
 
     setLogFile("log.txt");
 
-    // glfw: initialize and setup callbacks 
-    auto& w = gxb::SCR_WIDTH;
-    auto& h = gxb::SCR_HEIGHT;
-    
-	GLFWwindow* window = initGLFW(w, h, "Learn OpenGL ", framebuf_size_callback);
+    // glfw: initialize and setup callbacks
+	const auto & w = gxb::SCR_WIDTH;
+	const auto & h = gxb::SCR_HEIGHT;
+    GLFWwindow* window =
+	initGLFW(w, h, "Learn OpenGL ", framebuf_size_callback);
     glfwSetCursorPosCallback(window, mouse_callback);
     glfwSetScrollCallback(window, scroll_callback);
     glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
     glEnable(GL_DEPTH_TEST);
-	glfwSwapInterval(1);
+    glfwSwapInterval(1); // vsync
     logOpenGLInfo();
 
     auto level = gxb::load_level("test");
 
-    auto progOne = Shader(*gxb::shaderPath("3pos3color.vs"), *gxb::shaderPath("colorFromVertex.fs"));
+    auto progOne = Shader(*gxb::shaderPath("3pos3color.vs"),
+			  *gxb::shaderPath("colorFromVertex.fs"));
     int VAO = init_vertices();
 
     glm::mat4 model = glm::mat4(1.0f);
@@ -51,67 +56,64 @@ int main()
 
     // this code should be in the level
     glm::vec3 cubePositions[] = {
-        glm::vec3(0.0f, 0.0f, 0.0f),
-        glm::vec3(2.0f, 5.0f, -15.0f),
-        glm::vec3(-1.5f, -2.2f, -2.5f),
-        glm::vec3(-3.8f, -2.0f, -12.3f),
-        glm::vec3(2.4f, -0.4f, -3.5f),
-        glm::vec3(-1.7f, 3.0f, -7.5f),
-        glm::vec3(1.3f, -2.0f, -2.5f),
-        glm::vec3(1.5f, 2.0f, -2.5f),
-        glm::vec3(1.5f, 0.2f, -1.5f),
-        glm::vec3(-1.3f, 1.0f, -1.5f)
-    };
+	glm::vec3(0.0f, 0.0f, 0.0f),	glm::vec3(2.0f, 5.0f, -15.0f),
+	glm::vec3(-1.5f, -2.2f, -2.5f), glm::vec3(-3.8f, -2.0f, -12.3f),
+	glm::vec3(2.4f, -0.4f, -3.5f),	glm::vec3(-1.7f, 3.0f, -7.5f),
+	glm::vec3(1.3f, -2.0f, -2.5f),	glm::vec3(1.5f, 2.0f, -2.5f),
+	glm::vec3(1.5f, 0.2f, -1.5f),	glm::vec3(-1.3f, 1.0f, -1.5f)};
 
     // Game loop
     // -----------
     while (!glfwWindowShouldClose(window)) {
-        fr.UpdateTimes();
-        float deltaTime = fr.lastTimeInMs();
+	fr.UpdateTimes();
+	float deltaTime = fr.lastTimeInMs();
 
-        glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+	glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
 
-        processInput(window, camera);
+	processInput(window, camera, deltaTime);
 
-        progOne.use();
+	progOne.use();
 
-        // set transformations
-	    model = glm::mat4(1.0f);	
-        
-		view = camera.GetViewMatrix();
-        progOne.setMat4("view", view);
-        
-		projection = glm::perspective(glm::radians(camera.Zoom), 800.0f / 600.0f, 0.1f, 100.0f);
-        progOne.setMat4("projection", projection);
-        
-		// render
-        // ------
-        glClearColor(0.2f, 0.3f, 0.7f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+	// set transformations
+	model = glm::mat4(1.0f);
 
-        glBindVertexArray(VAO);
+	view = camera.GetViewMatrix();
+	progOne.setMat4("view", view);
 
-		
-        for (unsigned int i = 0; i < 10; i++) {
-            model = glm::mat4(1.0f);
-            model = glm::translate(model, cubePositions[i]);
-            float angle = 20.0f * i;
-            if ((i + 1) % 3 == 0) {
-                model = glm::rotate(model, glm::radians((float)glfwGetTime() * (angle + 1)), glm::vec3(1.0f, 0.3f, 0.5f));
-            } else {
-                model = glm::rotate(model, glm::radians(angle + 1), glm::vec3(1.0f, 0.3f, 0.5f));
-            }
-            progOne.setMat4("model", model);
-            glDrawArrays(GL_TRIANGLES, 0, 36);
-        }
+	projection = glm::perspective(glm::radians(camera.Zoom),
+				      800.0f / 600.0f, 0.1f, 100.0f);
+	progOne.setMat4("projection", projection);
 
-        glBindVertexArray(0); // no need to unbind it every time
+	// render
+	// ------
+	glClearColor(0.2f, 0.3f, 0.7f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
 
-        // glfw: swap buffers and poll IO events (keys pressed/released, mouse
-        // moved etc.)
-        // -------------------------------------------------------------------------------
-        glfwSwapBuffers(window);
-        glfwPollEvents();
+	glBindVertexArray(VAO);
+
+	for (unsigned int i = 0; i < 10; i++) {
+	    model = glm::mat4(1.0f);
+	    model = glm::translate(model, cubePositions[i]);
+	    float angle = 20.0f * i;
+	    if ((i + 1) % 3 == 0) {
+		model = glm::rotate(
+		    model, glm::radians((float)glfwGetTime() * (angle + 1)),
+		    glm::vec3(1.0f, 0.3f, 0.5f));
+	    } else {
+		model = glm::rotate(model, glm::radians(angle + 1),
+				    glm::vec3(1.0f, 0.3f, 0.5f));
+	    }
+	    progOne.setMat4("model", model);
+	    glDrawArrays(GL_TRIANGLES, 0, 36);
+	}
+
+	glBindVertexArray(0);  // no need to unbind it every time
+
+	// glfw: swap buffers and poll IO events (keys pressed/released, mouse
+	// moved etc.)
+	// -------------------------------------------------------------------------------
+	glfwSwapBuffers(window);
+	glfwPollEvents();
     }
 
     // optional: de-allocate all resources once they've outlived their purpose:
@@ -126,45 +128,42 @@ int main()
     return 0;
 }
 
-void logOpenGLInfo()
-{
+void logOpenGLInfo() {
     // Query GL
     int glMajVers, glMinVers;
     glGetIntegerv(GL_MAJOR_VERSION, &glMajVers);
     glGetIntegerv(GL_MINOR_VERSION, &glMinVers);
-    logPrintLn({ "OpenGL Version:", glMajVers, ".", glMinVers });
+    logPrintLn({"OpenGL Version:", glMajVers, ".", glMinVers});
 }
 
 // returns the VAO id
-unsigned int init_vertices()
-{
+unsigned int init_vertices() {
     // set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
     float vertices[] = {
-        -0.5f, -0.5f, -0.5f, 0.0f, 0.0f, 0.5f, -0.5f, -0.5f, 1.0f, 0.0f,
-        0.5f, 0.5f, -0.5f, 1.0f, 1.0f, 0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
-        -0.5f, 0.5f, -0.5f, 0.0f, 1.0f, -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
+	-0.5f, -0.5f, -0.5f, 0.0f, 0.0f, 0.5f,	-0.5f, -0.5f, 1.0f, 0.0f,
+	0.5f,  0.5f,  -0.5f, 1.0f, 1.0f, 0.5f,	0.5f,  -0.5f, 1.0f, 1.0f,
+	-0.5f, 0.5f,  -0.5f, 0.0f, 1.0f, -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
 
-        -0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
-        0.5f, 0.5f, 0.5f, 1.0f, 1.0f, 0.5f, 0.5f, 0.5f, 1.0f, 1.0f,
-        -0.5f, 0.5f, 0.5f, 0.0f, 1.0f, -0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+	-0.5f, -0.5f, 0.5f,  0.0f, 0.0f, 0.5f,	-0.5f, 0.5f,  1.0f, 0.0f,
+	0.5f,  0.5f,  0.5f,  1.0f, 1.0f, 0.5f,	0.5f,  0.5f,  1.0f, 1.0f,
+	-0.5f, 0.5f,  0.5f,  0.0f, 1.0f, -0.5f, -0.5f, 0.5f,  0.0f, 0.0f,
 
-        -0.5f, 0.5f, 0.5f, 1.0f, 0.0f, -0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
-        -0.5f, -0.5f, -0.5f, 0.0f, 1.0f, -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
-        -0.5f, -0.5f, 0.5f, 0.0f, 0.0f, -0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+	-0.5f, 0.5f,  0.5f,  1.0f, 0.0f, -0.5f, 0.5f,  -0.5f, 1.0f, 1.0f,
+	-0.5f, -0.5f, -0.5f, 0.0f, 1.0f, -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+	-0.5f, -0.5f, 0.5f,  0.0f, 0.0f, -0.5f, 0.5f,  0.5f,  1.0f, 0.0f,
 
-        0.5f, 0.5f, 0.5f, 1.0f, 0.0f, 0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
-        0.5f, -0.5f, -0.5f, 0.0f, 1.0f, 0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
-        0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+	0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 0.5f,	0.5f,  -0.5f, 1.0f, 1.0f,
+	0.5f,  -0.5f, -0.5f, 0.0f, 1.0f, 0.5f,	-0.5f, -0.5f, 0.0f, 1.0f,
+	0.5f,  -0.5f, 0.5f,  0.0f, 0.0f, 0.5f,	0.5f,  0.5f,  1.0f, 0.0f,
 
-        -0.5f, -0.5f, -0.5f, 0.0f, 1.0f, 0.5f, -0.5f, -0.5f, 1.0f, 1.0f,
-        0.5f, -0.5f, 0.5f, 1.0f, 0.0f, 0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
-        -0.5f, -0.5f, 0.5f, 0.0f, 0.0f, -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+	-0.5f, -0.5f, -0.5f, 0.0f, 1.0f, 0.5f,	-0.5f, -0.5f, 1.0f, 1.0f,
+	0.5f,  -0.5f, 0.5f,  1.0f, 0.0f, 0.5f,	-0.5f, 0.5f,  1.0f, 0.0f,
+	-0.5f, -0.5f, 0.5f,  0.0f, 0.0f, -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
 
-        -0.5f, 0.5f, -0.5f, 0.0f, 1.0f, 0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
-        0.5f, 0.5f, 0.5f, 1.0f, 0.0f, 0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
-        -0.5f, 0.5f, 0.5f, 0.0f, 0.0f, -0.5f, 0.5f, -0.5f, 0.0f, 1.0f
-    };
+	-0.5f, 0.5f,  -0.5f, 0.0f, 1.0f, 0.5f,	0.5f,  -0.5f, 1.0f, 1.0f,
+	0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 0.5f,	0.5f,  0.5f,  1.0f, 0.0f,
+	-0.5f, 0.5f,  0.5f,  0.0f, 0.0f, -0.5f, 0.5f,  -0.5f, 0.0f, 1.0f};
 
     unsigned int VBO, VAO;
     glGenVertexArrays(1, &VAO);
@@ -176,11 +175,12 @@ unsigned int init_vertices()
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float),
+			  (void*)0);
     glEnableVertexAttribArray(0);
 
     glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float),
-        (void*)(3 * sizeof(float)));
+			  (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
     // note that this is allowed, the call to glVertexAttribPointer registered
     // VBO as the vertex attribute's bound vertex buffer object so afterwards we
@@ -196,24 +196,25 @@ unsigned int init_vertices()
     return VAO;
 }
 
-void init_textures()
-{
+void init_textures() {
     unsigned int tex1;
     glGenTextures(1, &tex1);
     glBindTexture(GL_TEXTURE_2D, tex1);
     int width, height, nrChannels;
-    std::string filePath { gxb::texturePath + std::string("wooden_container.jpg") };
-    unsigned char* data = stbi_load(filePath.c_str(), &width, &height, &nrChannels, 0);
+    std::string filePath{gxb::texturePath +
+			 std::string("wooden_container.jpg")};
+    unsigned char* data =
+	stbi_load(filePath.c_str(), &width, &height, &nrChannels, 0);
 
     if (data) {
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB,
-            GL_UNSIGNED_BYTE, data);
-        glGenerateMipmap(GL_TEXTURE_2D);
-        stbi_image_free(data);
-        logPrintLn({ "texture loaded ( w:", width, "h:", height,
-            "nchan:", nrChannels, ")" });
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB,
+		     GL_UNSIGNED_BYTE, data);
+	glGenerateMipmap(GL_TEXTURE_2D);
+	stbi_image_free(data);
+	logPrintLn({"texture loaded ( w:", width, "h:", height,
+		    "nchan:", nrChannels, ")"});
     } else {
-        logErr(__FILE__, __LINE__, "failure to load texture data");
+	logErr(__FILE__, __LINE__, "failure to load texture data");
     }
 
     unsigned int tex2;
@@ -226,63 +227,63 @@ void init_textures()
     data = stbi_load(filePath.c_str(), &width, &height, &nrChannels, 0);
 
     if (data) {
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB,
-            GL_UNSIGNED_BYTE, data);
-        glGenerateMipmap(GL_TEXTURE_2D);
-        stbi_image_free(data);
-        logPrintLn({ "texture loaded ( w:", width, "h:", height,
-            "nchan:", nrChannels, ")" });
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB,
+		     GL_UNSIGNED_BYTE, data);
+	glGenerateMipmap(GL_TEXTURE_2D);
+	stbi_image_free(data);
+	logPrintLn({"texture loaded ( w:", width, "h:", height,
+		    "nchan:", nrChannels, ")"});
     } else {
-        logErr(__FILE__, __LINE__, "failure to load texture data");
+	logErr(__FILE__, __LINE__, "failure to load texture data");
     }
 }
 
 // process all input: query GLFW whether relevant keys are pressed/released
 // this frame and react accordingly
 // ---------------------------------------------------------------------------------------------------------
-void processInput(GLFWwindow* window, gxb::Camera& cam)
-{
-    const float cameraSpeed = 0.05f; // adjust accordingly
+void processInput(GLFWwindow* window, gxb::Camera& cam, float deltaTime) {
+    const float cameraSpeed = 0.05f;  // adjust accordingly
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true);
+	glfwSetWindowShouldClose(window, true);
 
     if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
-        cam.Position += glm::normalize(glm::cross(cam.Front, cam.Up)) * cameraSpeed;
+    cam.ProcessKeyboard(gxb::Camera_Movement::RIGHT, deltaTime);
 
     if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
-        cam.Position -= glm::normalize(glm::cross(cam.Front, cam.Up)) * cameraSpeed;
+    cam.ProcessKeyboard(gxb::Camera_Movement::LEFT, deltaTime);
+		
 
     if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
-        cam.Position += cameraSpeed * cam.Front;
+    cam.ProcessKeyboard(gxb::Camera_Movement::FORWARD, deltaTime);
 
     if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
-        cam.Position -= cameraSpeed * cam.Front;
+    cam.ProcessKeyboard(gxb::Camera_Movement::BACKWARD, deltaTime);
 
     if (glfwGetKey(window, GLFW_KEY_Z) == GLFW_PRESS)
-        __noop;
+	__noop;
 
     if (glfwGetKey(window, GLFW_KEY_X) == GLFW_PRESS)
-        __noop;
+	__noop;
 }
 
 // glfw: whenever the window size changed (by OS or user resize) this callback
 // function executes
 // ---------------------------------------------------------------------------------------------
-void framebuf_size_callback(GLFWwindow* window, int width, int height)
-{
+void framebuf_size_callback(GLFWwindow* window, int width, int height) {
     glViewport(0, 0, width, height);
 }
 
-void mouse_callback(GLFWwindow* window, double xpos, double ypos)
-{
+void mouse_callback(GLFWwindow* window, double xpos, double ypos) {
     if (firstMouse) {
-        lastX = (float)xpos;
-        lastY = (float)ypos;
-        firstMouse = false;
+	lastX = (float)xpos;
+	lastY = (float)ypos;
+	firstMouse = false;
     }
 
     float xoffset = (float)xpos - lastX;
-    float yoffset = lastY - (float)ypos; // reversed since y-coordinates go from bottom to top
+    float yoffset =
+	lastY -
+	(float)ypos;  // reversed since y-coordinates go from bottom to top
 
     lastX = (float)xpos;
     lastY = (float)ypos;
@@ -291,10 +292,9 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos)
 }
 
 GLFWwindow* initGLFW(unsigned int w,
-    unsigned int h,
-    const char* title,
-    GLFWframebuffersizefun fun)
-{
+		     unsigned int h,
+		     const char* title,
+		     GLFWframebuffersizefun fun) {
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -304,9 +304,9 @@ GLFWwindow* initGLFW(unsigned int w,
     // --------------------
     GLFWwindow* window = glfwCreateWindow(w, h, title, NULL, NULL);
     if (window == NULL) {
-        logPrintLn({ "Failed to create GLFW window" });
-        glfwTerminate();
-        return nullptr;
+	logPrintLn({"Failed to create GLFW window"});
+	glfwTerminate();
+	return nullptr;
     }
     glfwMakeContextCurrent(window);
     glfwSetFramebufferSizeCallback(window, fun);
@@ -314,15 +314,14 @@ GLFWwindow* initGLFW(unsigned int w,
     // glad: load all OpenGL function pointers
     // ---------------------------------------
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-        logPrintLn({ "Failed to initialize GLAD" });
-        return nullptr;
+	logPrintLn({"Failed to initialize GLAD"});
+	return nullptr;
     }
     return window;
 }
 
 // glfw: whenever the mouse scroll wheel scrolls, this callback is called
 // ----------------------------------------------------------------------
-void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
-{
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset) {
     camera.ProcessMouseScroll((float)yoffset);
 }
