001e7b8b3db73f58e31d11ffc503b9f0bbf50616
Blake Ba <blake.elink@gmail.com>
2021-04-28 16:03:08 -0400

    added bezier screenshot

--- editors/bezier_test.cpp
+++ editors/bezier_test.cpp
@@ -12,45 +12,75 @@ struct camPath {
     camPath() = delete;
     camPath(vector<vec3> control_points) { cps = control_points; }
     void createPathFromCps() {
-	for (int cpIdx = 0; cpIdx <= cps.size() - 3; cpIdx += 3) {
-		vec3 pt0, pt1, p2;
-	    auto [x, y, z, t] = {0.f, 0.f, 0.f, 0.f};
-	    x = (1 - t) * (1 - t) * pt0.x + 2 * (1 - t) * t * pt1.x + t * t * pt2.x;
-	    y = (1 - t) * (1 - t) * pt0.y + 2 * (1 - t) * t * pt1.y + t * t * pt2.y;
-	    z = (1 - t) * (1 - t) * pt0.z + 2 * (1 - t) * t * pt1.z + t * t * pt2.z;
+	// 0 1 2 3 4 5
+
+	for (int cpIdx = 0; cpIdx <= cps.size() - 3; cpIdx += 2) {
+	    const vec3& p0 = cps[cpIdx];
+	    const vec3& p1 = cps[cpIdx + 1];
+	    const vec3& p2 = cps[cpIdx + 2];
+	    float x, y, z, t;
+	    x = y = z = t = 0.0f;
+	    while (t < 1.0) {
+		x = (1 - t) * (1 - t) * p0.x + 2 * (1 - t) * t * p1.x +
+		    t * t * p2.x;
+		y = (1 - t) * (1 - t) * p0.y + 2 * (1 - t) * t * p1.y +
+		    t * t * p2.y;
+		z = (1 - t) * (1 - t) * p0.z + 2 * (1 - t) * t * p1.z +
+		    t * t * p2.z;
+		pts.push_back(vec3{x, y, z});
+		t += 0.10f;
+	    }
 	}
     }
     vector<vec3> cam_dir;
     vector<vec3> pts;
-   private:
     vector<vec3> cps;
 };
 
 class Example : public olc::PixelGameEngine {
    public:
-    Example() {
-	camPath cp{};
-	sAppName = "Example";
-    }
+    Example() { sAppName = "Example"; }
 
    public:
     bool OnUserCreate() override {
 	// Called once at the start, so create things here
+	vector<vec3> cps = {vec3{3.0f, 100.0f, 0.f},
+		vec3{50.0f, 120.f, 0.f},
+			    vec3{75.0f, 100.f, 0.f},
+			    vec3{100.0f, 85.f, 0.f},
+			    vec3{125.0f, 100.f, 0.f},
+			    vec3{175.0f, 110.f, 0.f},
+			    vec3{220.0f, 100.f, 0.f},
+			    vec3{240.0f, 85.f, 0.f},
+			    vec3{256.0f, 95.f, 0.f}};
+	path = new camPath(cps);
+	cout << "number of control points: " << path->cps.size() << endl;
+	path->createPathFromCps();
 	return true;
     }
 
     bool OnUserUpdate(float fElapsedTime) override {
-	// called once per frame
-	for (int x = 0; x < ScreenWidth(); x++)
-	    for (int y = 0; y < ScreenHeight(); y++)
-		Draw(x, y, olc::Pixel(0, 0, rand() % 255));
+
+	this->Clear(olc::Pixel(olc::DARK_GREY));
+
+    // draw path
+	for (const auto & pt : path->pts) {
+		Draw(pt.x, pt.y, olc::Pixel(255, 0, 10));
+	}
+	
+	// draw control points	
+	for (const auto & pt : path->cps) {
+		Draw(pt.x, pt.y, olc::Pixel(0, 255, 10));
+	}
+	
 	return true;
     }
+	camPath* path;
 };
 
 int main() {
     Example demo;
-    if (demo.Construct(256, 256, 4, 4))
+    if (demo.Construct(256, 256, 4, 4, false, true, false))
 	demo.Start();
 
     return 0;
