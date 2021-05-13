f13d6bc82253c58783ad0af2fc12c5095818f308
Blake Ba <blake.elink@gmail.com>
2021-05-13 00:45:12 -0400

    asdf

--- playground/sim.cpp
+++ playground/sim.cpp
@@ -25,7 +25,7 @@ class Example : public olc::PixelGameEngine {
    public:
     bool OnUserCreate() override {
 	// Called once at the start, so create things here
-	projected_points = vector<v2>(3);
+	projected_points = vector<v2>(points.size());
 	return true;
     }
 
@@ -33,7 +33,9 @@ class Example : public olc::PixelGameEngine {
 	// create projection
 	auto points_sz = points.size();
 	for (int i = 0; i < points_sz; i++) {
-		const auto & pt = points[i];
+		const auto & pt = glm::vec3(0,0,0);
+		model = glm::mat4(1.0f);
+		model = glm::translate(model, points[i]);
 	    auto tmp_v4 = projection * view * model * v4(pt.x, pt.y, pt.z, 1.0);
 		projected_points[i].x = tmp_v4.x;
 		projected_points[i].y = tmp_v4.y;
@@ -47,7 +49,7 @@ class Example : public olc::PixelGameEngine {
 	return true;
     }
     /* GAME VARS */
-    vector<v3> points{{0, 1, -1}, {0, 2, -3}, {0, 3, -5}};
+    vector<v3> points{{0, 1, -1}, {0, 2, -3}, {0, 3, -5}, {0,4, 7}};
     vector<v2> projected_points;
     m44 projection;
     m44 view;
