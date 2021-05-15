c322bd856e9b4ed30611ced9fccfecc0350562d2
Blake Ba <blake.elink@gmail.com>
2021-05-15 09:36:32 -0400

    working on octree

--- playground/octree.h
+++ playground/octree.h
@@ -1,204 +1,147 @@
-//
-//  Octree.h
-//
-//  Created by Eduardo Poyart on 6/4/12.
-//
+#pragma once
+#include <cstddef>
+#include <vector>
+#include "OctreePoint.h"
 
-/*
-Copyright (c) 2012, Eduardo Poyart.
-All rights reserved.
+	/**!
+	 *
+	 */
+	class Octree {
+		// Physical position/size. This implicitly defines the bounding 
+		// box of this node
+		Vec3 origin;         //! The physical center of this node
+		Vec3 halfDimension;  //! Half the width/height/depth of this node
 
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are
-met:
+		// The tree has up to eight children and can additionally store
+		// a point, though in many applications only, the leaves will store data.
+		Octree *children[8]; //! Pointers to child octants
+		OctreePoint *data;   //! Data point to be stored at a node
 
-* Redistributions of source code must retain the above copyright
-notice, this list of conditions and the following disclaimer.
-* Redistributions in binary form must reproduce the above
-copyright notice, this list of conditions and the following disclaimer
-in the documentation and/or other materials provided with the
-distribution.
+		/*
+				Children follow a predictable pattern to make accesses simple.
+				Here, - means less than 'origin' in that dimension, + means greater than.
+				child:	0 1 2 3 4 5 6 7
+				x:      - - - - + + + +
+				y:      - - + + - - + +
+				z:      - + - + - + - +
+		 */
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
+		public:
+		Octree(const Vec3& origin, const Vec3& halfDimension) 
+			: origin(origin), halfDimension(halfDimension), data(NULL) {
+				// Initially, there are no children
+				for(int i=0; i<8; ++i) 
+					children[i] = NULL;
+			}
 
-#ifndef Octree_h
-#define Octree_h
+		Octree(const Octree& copy)
+			: origin(copy.origin), halfDimension(copy.halfDimension), data(copy.data) {
 
-#include <assert.h>
+			}
 
-#define COMPUTE_SIDE(i, bit, p, mid, newMin, newMax) \
-if (p >= mid)         \
-{                     \
-    i |= bit;         \
-    newMin = mid;     \
-}                     \
-else                  \
-{                     \
-    newMax = mid;     \
-}
+		~Octree() {
+			// Recursively destroy octants
+			for(int i=0; i<8; ++i) 
+				delete children[i];
+		}
 
+		// Determine which octant of the tree would contain 'point'
+		int getOctantContainingPoint(const Vec3& point) const {
+			int oct = 0;
+			if(point.x >= origin.x) oct |= 4;
+			if(point.y >= origin.y) oct |= 2;
+			if(point.z >= origin.z) oct |= 1;
+			return oct;
+		}
 
-template <class N>
-class Octree
-{
-protected:
-    struct Point
-    {
-        float x; 
-        float y; 
-        float z;
-        Point(const Point& p2): x(p2.x), y(p2.y), z(p2.z) {}
-        Point& operator=(const Point& p2) { x = p2.x; y = p2.y; z = p2.z; return *this;}
-        Point(float in_x, float in_y, float in_z): x(in_x), y(in_y), z(in_z) {}
-        Point(const float p2[3]): x(p2[0]), y(p2[1]), z(p2[2]) {}
-        operator float*() { return &x; }
-        operator const float*() const { return &x; }
-        Point operator+(const Point& p2) const { return Point(x+p2.x, y+p2.y, z+p2.z); }
-        Point operator-(const Point& p2) const { return Point(x-p2.x, y-p2.y, z-p2.z); }
-        Point operator*(float f) const { return Point(x*f, y*f, z*f); }
-        bool operator< (const Point& p2) const { return x <  p2.x && y <  p2.y && z <  p2.z; }
-        bool operator>=(const Point& p2) const { return x >= p2.x && y >= p2.y && z >= p2.z; }
-    };
-    
-    struct OctreeNode
-    {
-        N _nodeData;
-        OctreeNode* _children[8];
-        OctreeNode()
-        {
-            for (int i = 0; i < 8; i++)
-                _children[i] = 0;
-        }
-        virtual ~OctreeNode()
-        {
-            for (int i = 0; i < 8; i++)
-                if (_children[i])
-                    delete _children[i];
-        }
-    };
-    
-    Point _min;
-    Point _max;
-    Point _cellSize;
-    OctreeNode* _root;
-    
-public:
-    Octree(float min[3], float max[3], float cellSize[3]): _min(min), _max(max), _cellSize(cellSize), _root(0) {}
-    virtual ~Octree() { delete _root; }
-    
-    class Callback
-    {
-    public:
-        // Return value: true = continue; false = abort.
-        virtual bool operator()(const float min[3], const float max[3], N& nodeData) = 0;
-    };
-    
-    N& getCell(const float pos[3], Callback* callback = NULL)
-    {
-        Point ppos(pos);
-        assert(ppos >= _min && ppos < _max);
-        Point currMin(_min);
-        Point currMax(_max);
-        Point delta = _max - _min;
-        if (!_root)
-            _root = new OctreeNode();
-        OctreeNode* currNode = _root;
-        while (delta >= _cellSize)
-        {
-            bool shouldContinue = true;
-            if (callback)
-                shouldContinue = callback->operator()(currMin, currMax, currNode->_nodeData);
-            if (!shouldContinue)
-                break;
-            Point mid = (delta * 0.5f) + currMin;
-            Point newMin(currMin);
-            Point newMax(currMax);
-            int index = 0;
-            COMPUTE_SIDE(index, 1, ppos.x, mid.x, newMin.x, newMax.x)
-            COMPUTE_SIDE(index, 2, ppos.y, mid.y, newMin.y, newMax.y)
-            COMPUTE_SIDE(index, 4, ppos.z, mid.z, newMin.z, newMax.z)
-            if (!(currNode->_children[index]))
-                currNode->_children[index] = new OctreeNode();
-            currNode = currNode->_children[index];
-            currMin = newMin;
-            currMax = newMax;
-            delta = currMax - currMin;
-        }
-        return currNode->_nodeData;
-    }
-    
-    void traverse(Callback* callback)
-    {
-        assert(callback);
-        traverseRecursive(callback, _min, _max, _root);
-    }
-    
-    void clear()
-    {
-        delete _root;
-        _root = NULL;
-    }
-    
-    class Iterator
-    {
-    public:
-        Iterator getChild(int i)
-        {
-            return Iterator(_currNode->_children[i]);
-        }
-        N* getData()
-        {
-            if (_currNode)
-                return &_currNode->_nodeData;
-            else return NULL;
-        }
-    protected:
-        OctreeNode* _currNode;
-        Iterator(OctreeNode* node): _currNode(node) {}
-        friend class Octree;
-    };
-    
-    Iterator getIterator()
-    {
-        return Iterator(_root);
-    }
-    
-protected:
-    void traverseRecursive(Callback* callback, const Point& currMin, const Point& currMax, OctreeNode* currNode)
-    {
-        if (!currNode)
-            return;
-        bool shouldContinue = callback->operator()(currMin, currMax, currNode->_nodeData);
-        if (!shouldContinue)
-            return;
-        Point delta = currMax - currMin;
-        Point mid = (delta * 0.5f) + currMin;
-        traverseRecursive(callback, currMin, mid, currNode->_children[0]);
-        traverseRecursive(callback, Point(mid.x, currMin.y, currMin.z), 
-                                    Point(currMax.x, mid.y, mid.z), currNode->_children[1]);
-        traverseRecursive(callback, Point(currMin.x, mid.y, currMin.z), 
-                                    Point(mid.x, currMax.y, mid.z), currNode->_children[2]);
-        traverseRecursive(callback, Point(mid.x, mid.y, currMin.z), 
-                                    Point(currMax.x, currMax.y, mid.z), currNode->_children[3]);
-        traverseRecursive(callback, Point(currMin.x, currMin.y, mid.z), 
-                                    Point(mid.x, mid.y, currMax.z), currNode->_children[4]);
-        traverseRecursive(callback, Point(mid.x, currMin.y, mid.z), 
-                                    Point(currMax.x, mid.y, currMax.z), currNode->_children[5]);
-        traverseRecursive(callback, Point(currMin.x, mid.y, mid.z), 
-                                    Point(mid.x, currMax.y, currMax.z), currNode->_children[6]);
-        traverseRecursive(callback, mid, currMax, currNode->_children[7]);
-    }    
-};
+		bool isLeafNode() const {
+			// This is correct, but overkill. See below.
+			/*
+				 for(int i=0; i<8; ++i)
+				 if(children[i] != NULL) 
+				 return false;
+				 return true;
+			 */
 
-#endif
+			// We are a leaf iff we have no children. Since we either have none, or 
+			// all eight, it is sufficient to just check the first.
+			return children[0] == NULL;
+		}
+
+		void insert(OctreePoint* point) {
+			// If this node doesn't have a data point yet assigned 
+			// and it is a leaf, then we're done!
+			if(isLeafNode()) {
+				if(data==NULL) {
+					data = point;
+					return;
+				} else {
+					// We're at a leaf, but there's already something here
+					// We will split this node so that it has 8 child octants
+					// and then insert the old data that was here, along with 
+					// this new data point
+
+					// Save this data point that was here for a later re-insert
+					OctreePoint *oldPoint = data;
+					data = NULL;
+
+					// Split the current node and create new empty trees for each
+					// child octant.
+					for(int i=0; i<8; ++i) {
+						// Compute new bounding box for this child
+						Vec3 newOrigin = origin;
+						newOrigin.x += halfDimension.x * (i&4 ? .5f : -.5f);
+						newOrigin.y += halfDimension.y * (i&2 ? .5f : -.5f);
+						newOrigin.z += halfDimension.z * (i&1 ? .5f : -.5f);
+						children[i] = new Octree(newOrigin, halfDimension*.5f);
+					}
+
+					// Re-insert the old point, and insert this new point
+					// (We wouldn't need to insert from the root, because we already
+					// know it's guaranteed to be in this section of the tree)
+					children[getOctantContainingPoint(oldPoint->getPosition())]->insert(oldPoint);
+					children[getOctantContainingPoint(point->getPosition())]->insert(point);
+				}
+			} else {
+				// We are at an interior node. Insert recursively into the 
+				// appropriate child octant
+				int octant = getOctantContainingPoint(point->getPosition());
+				children[octant]->insert(point);
+			}
+		}
+
+		// This is a really simple routine for querying the tree for points
+		// within a bounding box defined by min/max points (bmin, bmax)
+		// All results are pushed into 'results'
+		void getPointsInsideBox(const Vec3& bmin, const Vec3& bmax, std::vector<OctreePoint*>& results) {
+			// If we're at a leaf node, just see if the current data point is inside
+			// the query bounding box
+			if(isLeafNode()) {
+				if(data!=NULL) {
+					const Vec3& p = data->getPosition();
+					if(p.x>bmax.x || p.y>bmax.y || p.z>bmax.z) return;
+					if(p.x<bmin.x || p.y<bmin.y || p.z<bmin.z) return;
+					results.push_back(data);
+				}
+			} else {
+				// We're at an interior node of the tree. We will check to see if
+				// the query bounding box lies outside the octants of this node.
+				for(int i=0; i<8; ++i) {
+					// Compute the min/max corners of this child octant
+					Vec3 cmax = children[i]->origin + children[i]->halfDimension;
+					Vec3 cmin = children[i]->origin - children[i]->halfDimension;
+
+					// If the query rectangle is outside the child's bounding box, 
+					// then continue
+					if(cmax.x<bmin.x || cmax.y<bmin.y || cmax.z<bmin.z) continue;
+					if(cmin.x>bmax.x || cmin.y>bmax.y || cmin.z>bmax.z) continue;
+
+					// At this point, we've determined that this child is intersecting 
+					// the query bounding box
+					children[i]->getPointsInsideBox(bmin,bmax,results);
+				} 
+			}
+		}
+
+	};
+
