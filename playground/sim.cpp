a52675842771dc62e8037a9e89754a1a26303146
Blake Ba <blake.elink@gmail.com>
2021-05-14 20:55:24 -0400

    working on octree

--- playground/sim.cpp
+++ playground/sim.cpp
@@ -30,47 +30,47 @@ class Example : public olc::PixelGameEngine {
     }
 
     bool OnUserUpdate(float fElapsedTime) override {
+	totElapsedTime += fElapsedTime;
+	Clear(olc::BLACK);
 	// create projection
 	auto points_sz = points.size();
 	for (int i = 0; i < points_sz; i++) {
-		auto pt = points[i];
-		pt.z += 10.0f;
-		model = glm::mat4(1.0f);
-		model = glm::rotate(model, fElapsedTime * 0.4f, glm::vec3(1.0,1.0,1.0));
+	    auto pt = points[i];
+	    model = glm::mat4(1.0f);
+	    model = glm::rotate(model, totElapsedTime * 0.2f,
+				glm::vec3(0.5, 0.5, 0.5));
 	    auto tmp_v4 = projection * view * model * v4(pt.x, pt.y, pt.z, 1.0);
-		projected_points[i].x = tmp_v4.x;
-		projected_points[i].y = tmp_v4.y;
-		// shift and scale
-		projected_points[i].x *= 25.0f;
-		projected_points[i].y *= 25.0f;
-		projected_points[i].x += g_WIDTH / 3.0f;
-		projected_points[i].y += g_HEIGHT / 3.0f;
+	    projected_points[i].x = tmp_v4.x;
+	    projected_points[i].y = tmp_v4.y;
+	    // shift and scale
+	    projected_points[i].x *= 18.0f;
+	    projected_points[i].y *= 18.0f;
+	    projected_points[i].x += g_WIDTH / 2.0f;
+	    projected_points[i].y += g_HEIGHT / 2.0f;
 	}
 
 	// Called once per frame, draws random coloured pixels
-	for (int i = 0; i < points_sz; i++) {
-	const auto & pt = projected_points[i];
-	Draw(pt.x, pt.y,
-		     olc::BLUE);
+	for (int i = 0; i < points_sz - 2; i+=2) {
+	    const auto& pt = projected_points[i];
+	    const auto& pt2 = projected_points[i + 1];
+	    const auto& pt3 = projected_points[i + 2];
+	    DrawTriangle(pt.x, pt.y, pt2.x, pt2.y, pt3.x, pt3.y, olc::WHITE);
 	}
 	return true;
     }
     /* GAME VARS */
+    float totElapsedTime;
     vector<v3> points{
 
-{-1,-1,1}, {1,-1,1}, {1,1,1},
- {-1,-1,1}, {1,1,1}, {-1,1,1},
- {1,-1,1}, {1,-1,-1}, {1,1,-1},
- {1,-1,1}, {1,1,-1}, {1,1,1},
- {1,-1,-1}, {-1,-1,-1}, {-1,1,-1},
- {1,-1,-1}, {-1,1,-1}, {1,1,-1},
- {-1,-1,-1}, {-1,-1,1}, {-1,1,1},
- {-1,-1,-1}, {-1,1,1}, {-1,1,-1},
- {-1,1,1}, {1,1,1}, {1,1,-1},
- {-1,1,1}, {1,1,-1}, {-1,1,-1},
- {1,-1,1}, {-1,-1,-1}, {1,-1,-1},
- {1,-1,1}, {-1,-1, 1}, {-1,-1,-1},
- };
+	{-1, -1, 1},  {1, -1, 1},   {1, 1, 1},	 {-1, -1, 1},  {1, 1, 1},
+	{-1, 1, 1},   {1, -1, 1},   {1, -1, -1}, {1, 1, -1},   {1, -1, 1},
+	{1, 1, -1},   {1, 1, 1},    {1, -1, -1}, {-1, -1, -1}, {-1, 1, -1},
+	{1, -1, -1},  {-1, 1, -1},  {1, 1, -1},	 {-1, -1, -1}, {-1, -1, 1},
+	{-1, 1, 1},   {-1, -1, -1}, {-1, 1, 1},	 {-1, 1, -1},  {-1, 1, 1},
+	{1, 1, 1},    {1, 1, -1},   {-1, 1, 1},	 {1, 1, -1},   {-1, 1, -1},
+	{1, -1, 1},   {-1, -1, -1}, {1, -1, -1}, {1, -1, 1},   {-1, -1, 1},
+	{-1, -1, -1},
+    };
 
     vector<v2> projected_points;
     m44 projection;
@@ -86,7 +86,7 @@ int main() {
     demo.projection = glm::perspective(
 	(float)PI / 2.0f, (float)g_WIDTH / (float)g_HEIGHT, 0.1f, 100.0f);
 
-    if (demo.Construct(g_WIDTH, g_HEIGHT, 4, 4))
+    if (demo.Construct(g_WIDTH, g_HEIGHT, 4, 4, false, true))
 	demo.Start();
     return 0;
 }
