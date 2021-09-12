ad9cd46029b0e1ce17567df3b1f609c4085d4fd6
Blake Ba <blake.elink@gmail.com>
2021-09-12 08:33:37 -0400

    deleted dead code; started level_gen.h

--- src/update.h
+++ src/update.h
@@ -1,16 +1,1 @@
 #pragma once
-//#include <magic_enum.h>
-
-
-
-
-//
-//int main() {
-//  FsmBinary fsm;
-//
-//  std::cout << "current state=" << magic_enum::enum_name(fsm.current) << " type=" << magic_enum::enum_integer(fsm.current) << std::endl;
-//  fsm.current = FsmBinary::States::neg;
-//  std::cout << "current state=" << magic_enum::enum_name(fsm.current) << " type=" << magic_enum::enum_integer(fsm.current) << std::endl;
-//
-//  return 0;
-//}
